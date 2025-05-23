2e669d8b21682b8051918e3a34bdbfbbd1712f2c
Gaurav Agerwala <gauravagerwala@gmail.com>
2025-05-22 23:35:34 -0700

    updating macos script

--- assets/scripts/mac_deps.sh
+++ assets/scripts/mac_deps.sh
@@ -9,14 +9,17 @@ echo ""
 echo "[1/3] Checking for Homebrew..."
 if ! command -v brew &> /dev/null; then
     echo "Homebrew not found. Installing Homebrew..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-    
+    curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
+    pwd
+    ls 
+
+    eval "$(bin/brew shellenv)"
     # Add Homebrew to PATH for this session
-    if [[ $(uname -m) == 'arm64' ]]; then
-        eval "$(/opt/homebrew/bin/brew shellenv)"
-    else
-        eval "$(/usr/local/bin/brew shellenv)"
-    fi
+    # if [[ $(uname -m) == 'arm64' ]]; then
+    #     eval "$(homebrew/bin/brew shellenv)"
+    # else
+    #     eval "$(/usr/local/bin/brew shellenv)"
+    # fi
 else
     echo "Homebrew is already installed."
 fi
@@ -29,17 +32,18 @@ if command -v python3 &> /dev/null; then
     PYTHON_MAJOR=$(echo $PYTHON_VERSION | cut -d. -f1)
     PYTHON_MINOR=$(echo $PYTHON_VERSION | cut -d. -f2)
     
-    if [ "$PYTHON_MAJOR" -ge 3 ] && [ "$PYTHON_MINOR" -ge 10 ]; then
+    if [ "$PYTHON_MAJOR" -ge 3 ] && [ "$PYTHON_MINOR" -ge 11 ]; then
         echo "Python $PYTHON_VERSION is already installed and up-to-date."
     else
-        echo "Python version $PYTHON_VERSION is outdated. Installing Python 3.10+..."
-        brew install python@3.10
-        echo "Python 3.10+ installed successfully."
+        echo "Python version $PYTHON_VERSION is outdated. Installing Python 3.11+..."
+        brew install python@3.11
+        echo "Python 3.11+ installed successfully."
     fi
 else
-    echo "Python not found. Installing Python 3.10+..."
-    brew install python@3.10
-    echo "Python 3.10+ installed successfully."
+    echo "Python not found. Installing Python 3.11+..."
+    brew install python@3.11
+    echo "Python 3.11+ installed successfully."
+    echo "Python version: $(python3 --version)"
 fi
 
 # Install dependencies
